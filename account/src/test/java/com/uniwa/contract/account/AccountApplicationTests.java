797e2761af9cc88629293f5135c67ba752db9896
m.nikolopoulou <m.nikolopoulou@accenture.com>
2023-07-08 00:17:52 +0300

    unit tests

--- account/src/test/java/com/uniwa/contract/account/AccountApplicationTests.java
+++ account/src/test/java/com/uniwa/contract/account/AccountApplicationTests.java
@@ -29,7 +29,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 class AccountApplicationTests {
 
-
 	MockMvc mockMvc;
 
 	@Autowired
