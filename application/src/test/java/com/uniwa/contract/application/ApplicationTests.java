5a5d65e832b1cf186713c33b0a55889032d5d4ce
m.nikolopoulou <m.nikolopoulou@accenture.com>
2023-07-25 11:59:41 +0300

    new unit test

--- application/src/test/java/com/uniwa/contract/application/ApplicationTests.java
+++ application/src/test/java/com/uniwa/contract/application/ApplicationTests.java
@@ -1,30 +1,60 @@
 package com.uniwa.contract.application;
 
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import com.uniwa.contract.application.client.AccountClient;
+import com.uniwa.contract.application.controller.ApplicationController;
+import com.uniwa.contract.application.model.AccountDetails;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@EmbeddedKafka(topics = {"application.submitted.topic","application.processed.topic","application.completed.topic","application.canceled.topic"},
+		bootstrapServersProperty = "spring.kafka.bootstrap-servers")
+@DirtiesContext
+@ExtendWith(SpringExtension.class)
 class ApplicationTests {
 
-	@ClassRule
-	public static EmbeddedKafkaRule broker = new EmbeddedKafkaRule(1,
-			false, "application.submitted.topic", "application.processed.topic",
-			"application.completed.topic", "application.canceled.topic");
+	MockMvc mockMvc;
 
-	@BeforeClass
-	public static void setup() {
-		System.setProperty("spring.kafka.bootstrap-servers",
-				broker.getEmbeddedKafka().getBrokersAsString());
+	@Autowired
+	ApplicationController applicationController;
+
+	@MockBean
+	AccountClient accountClient;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(applicationController).build();
 	}
 
 	@Test
-	void contextLoads() {
+	@SneakyThrows
+	void createTest() {
+
+		ResponseEntity<AccountDetails> accountDetails = new ResponseEntity<>(AccountDetails.builder()
+				.userId("testId").email("test@test.gr").fullName("test test").build(), HttpStatus.OK);
+		Mockito.when(accountClient.accountDetails(Mockito.any(String.class)))
+				.thenReturn(accountDetails);
+
+		mockMvc.perform(post("/application/v1/test")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());
 	}
 
 }
